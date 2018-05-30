c8f7fdb2da82fb21cb6700d92336f5dc4ef452ce
Dowdle <thedowdle@byui.edu>
2018-05-30 17:00:20 -0600

    Prompts for setup

--- setUp.js
+++ setUp.js
@@ -1,11 +1,24 @@
-const enquirer = require('enquirer');
+const Enquirer = require('enquirer');
+const enquirer = new Enquirer();
 const fs = require('fs');
 
 function setUp() {
-    let question = [{
+    let questions = [{
+        name: 'API_Key',
         type: 'input',
-        name: 'API_key',
-        message: 'Please enter your API key'
+        message: 'What is your API Key?'
+    },
+    {
+        name: 'path',
+        type: 'input',
+        message: 'Where will the course to be saved?'
     }]
-    enquirer.prompt()
+
+    enquirer.ask(questions)
+        .then(answers => {
+            let key = answers.API_Key;
+            let path = answers.path;
+        })
+        .catch(console.error);
 }
+setUp();

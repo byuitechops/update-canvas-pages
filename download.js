f417159793b86971de9be52821b2294477d623b5
Colter-Hammer <therondowdle7@gmail.com>
2018-06-07 15:33:14 -0600

    Download page, and package.json

---
+++ download.js
@@ -0,0 +1,45 @@
+const Enquirer = require('enquirer');
+const enquirer = new Enquirer;
+const fs = require('fs');
+const canvas = require('canvas-api-wrapper');
+const setup = require('./setUp.js');
+const chalk = require('chalk');
+
+// Use Enquirer to get the course ID from the user
+function getCourseID() {
+    return new Promise((resolve, reject) => {
+        enquirer.question('course_ID', 'ID of the course you want to download', { default: 12845 });
+        enquirer.ask('course_ID')
+            .then(answers => {
+                resolve(answers.course_ID);
+            })
+            .catch(reject);
+
+    })
+};
+
+function writeToHD(pages, path, course_ID = 0) {
+    return new Promise((resolve, reject) => {
+        if (course_ID === 0) {
+            for (let page in pages) {
+                fs.writeFile(page, `${page.page_id} | ${page.title}`, err => {
+                    if (err) throw err;
+                });
+            }
+        }
+        else {
+            fs.mkdir()
+        }
+    })
+}
+
+async function main() {
+    let course_ID = await getCourseID();
+    console.log(course_ID);
+    let path = await setup(canvas);
+    writeToHD(course_ID)
+    let pages = await canvas.getCourse(course_ID).pages.get();
+    // writeToHD(pages, path);
+}
+
+main();

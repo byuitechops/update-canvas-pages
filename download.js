0ddadbb7ae0c7ac3e7580ab78b5efef46105a39a
Dowdle <thedowdle@byui.edu>
2018-07-02 16:59:12 -0600

    Writes new directory for each course. New file for each page.

--- download.js
+++ download.js
@@ -1,17 +1,16 @@
-const Enquirer = require('enquirer');
-const enquirer = new Enquirer;
-const fs = require('fs');
-const canvas = require('canvas-api-wrapper');
-const setup = require('./setUp.js').main;
-const chalk = require('chalk');
-const path = require('path');
-const handleErrors = require('./setup.js').errorHandling;
+const Enquirer = require('enquirer'),
+    enquirer = new Enquirer,
+    fs = require('fs'),
+    canvas = require('canvas-api-wrapper'),
+    setup = require('./setUp.js').main,
+    chalk = require('chalk'),
+    path = require('path'),
+    handleErrors = require('./setup.js').errorHandling;
 
-// Use Enquirer to get the course ID from the user
 function getCourseID() {
     return new Promise((resolve, reject) => {
         enquirer.question('course_ID', 'ID of the course you want to download', {
-            default: 16896
+            default: 16786
         });
         enquirer.ask('course_ID')
             .then(answers => {
@@ -21,34 +20,10 @@ function getCourseID() {
     });
 }
 
-/**
- * Checks if the directory exists.
- * Creates it if it doesn't.
- * 
- * Code drawn from StackOverflow: https://stackoverflow.com/a/40385651
- * 
- * @param {String} pathName 
- */
-function createDirectory(pathName) {
-    function isDir(dpath) {
-        try {
-            return fs.lstatSync(dpath).isDirectory();
-        } catch (e) {
-            return false;
-        }
-    }
-
-    pathName = path.normalize(pathName).split(path.sep);
-    pathName.forEach((sdir, index) => {
-        var pathInQuestion = pathName.slice(0, index + 1).join(path.sep);
-        if (!isDir(pathInQuestion) && pathInQuestion) fs.mkdirSync(pathInQuestion);
-    });
-}
 
 function verifyPath(dirPath) {
     let homeDir = require('os').homedir();
     let pathToDir = path.join(homeDir, dirPath);
-    console.log(pathToDir);
 
     /**
      * Checks if the directory exists.
@@ -88,12 +63,17 @@ function writeFile(path, fileGuts) {
     }
     console.log(`${path} written`);
 }
+
 /**
  * 
  * @param {object} toBeWritten 
  */
 function createFileName(name, number) {
-    return `${name.replace(/\s/g, '_')}_${number}`;
+    console.log(chalk.blue(name));
+    return `${name
+        .trim()
+        .replace(/-|\s/g, '_')
+        .replace(/\W/g, '')}-${number}`;
 }
 
 async function main() {
@@ -102,18 +82,17 @@ async function main() {
     let course = await canvas.getCourse(course_ID).get();
     let courseName = createFileName(course.course_code, course.id);
     console.log(path.join(dirPath, courseName));
-    // let pages = await course.pages.getComplete();
+    let pages = await course.pages.getComplete();
     let fullPath = verifyPath(path.join(dirPath, courseName));
-    // console.log(pages[0].title.replace(/\s/g, ''));
-    // let html = pages.map(page => {
-    //     let fileName = createFileName(page.title, page.page_id);
-    //     console.log(page);
-    //     return page.getHtml();
-    // });
+    // console.log(chalk.blue(pages[0].title));
+    // console.log(chalk.red(pages[0].title.replace(/-/g, '_').replace(/\W/g, '')));
 
-    // writeFile(course);
-    // writeFile(pages);
-    // console.log(html);
+    pages.map(page => {
+        let fileName = createFileName(page.title, page.page_id);
+        let filePath = path.join(fullPath, fileName);
+        writeFile(`${filePath}.html`, page.getHtml());
+        //     return page.getHtml();
+    });
 }
 
 main();

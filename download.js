a61e521d1a3f853071f3c3b677d88bdb98308cbe
Colter-Hammer <therondowdle7@gmail.com>
2018-08-29 13:43:53 -0600

    Sanitize file names

--- download.js
+++ download.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
-const cheerio = require('cheerio');
-const Enquirer = require('enquirer'),
+const cheerio = require('cheerio'),
+    Enquirer = require('enquirer'),
     enquirer = new Enquirer,
     fs = require('fs'),
     canvas = require('canvas-api-wrapper'),
     setup = require('./setUp.js').main,
     path = require('path'),
+    chalk = require('chalk'),
     handleErrors = require('./setup.js').errorHandling,
     npmWriteFile = require('write'),
-    camelCase = require('camelcase');
+    camelCase = require('camelcase'),
+    validator = require('validator'),
+    md5 = require('md5'),
+    fileExists = require('file-exists');
 enquirer.register('radio', require('prompt-radio'));
 
 function getCourseID() {
@@ -50,7 +54,7 @@ function verifyPath(coursePath) {
 
         pathName = path.normalize(pathName).split(path.sep);
         pathName.forEach((sdir, index) => {
-            var pathInQuestion = pathName.slice(0, index + 1).join(path.sep);
+            let pathInQuestion = pathName.slice(0, index + 1).join(path.sep);
             if (!isDir(pathInQuestion) && pathInQuestion) {
                 fs.mkdirSync(pathInQuestion);
             }
@@ -63,17 +67,57 @@ function verifyPath(coursePath) {
 
 
 /**
- * 
+ * 1. Sanitizes fileName
+ * 2. Removes spaces and camelCases
+ * 3. Either adds course/page ID, or returns just name
  * @param {object} toBeWritten 
  */
 function createFileName(name, number, fullCourse) {
-    // This needs to be run every time
-    name = `${camelCase(name)
-        .replace(/\W/g, '')}`;
+
+    /**
+     * Removes characters that are not allowed in URL
+     * You can change what is and isn't allowed by changeing `notAllowed`
+     *  * Uses regex, so escape characters as needed
+     * @param {String} fileName 
+     */
+    function sanitizeFileName(fileName) {
+        let notAllowed = ';/?:@&=+$,';
+        return validator.blacklist(fileName, notAllowed);
+    }
+
+    let sanitized = sanitizeFileName(name);
+    name = `${camelCase(sanitized)}`;
+    console.log(name);
     // This needs to be run if the 'fullcourse' option is selected false
     if (!fullCourse) name = `${name}-${number}`;
     return name;
 }
+
+/**
+ * Compares hash of new file to any existing file of the same name
+ * @param {String} page 
+ * @param {String} filePath 
+ */
+function checkContents(page, filePath) {
+    return new Promise((resolve, reject) => {
+        console.log(chalk.blue('FILE PATH: '), filePath);
+        fileExists(filePath, (err, exists) => {
+            if (!exists) {
+                resolve(true);
+            } else {
+                fs.readFile(filePath, 'utf-8', (err, oldFile) => {
+                    if (err) reject(err);
+                    let oldFileHash = md5(oldFile);
+                    let newFileHash = md5(page);
+                    if (oldFileHash === newFileHash) resolve(false);
+                    else resolve(true);
+                });
+            }
+
+        });
+    });
+}
+
 
 async function main() {
     let dirPath = await setup(canvas);
@@ -88,15 +132,23 @@ async function main() {
     pages.map(page => {
         let htmlString = page.getHtml();
         if (fullCourse) {
-            var $ = cheerio.load(htmlString);
+            let $ = cheerio.load(htmlString);
             // Potential if(want the things removed) {run code below};
             htmlString = $('link, script').remove();
             htmlString = $.html();
         }
         let fileName = createFileName(page.title, page.page_id, fullCourse);
-        let filePath = path.join(fullPath, fileName);
-        console.log(filePath);
-        npmWriteFile(`${filePath}.html`, htmlString);
+        let filePath = `${path.join(fullPath, fileName)}.html`;
+        checkContents(htmlString, filePath)
+            .then(valid => {
+                if (valid) {
+                    console.log(chalk.blueBright('Write file'));
+                    npmWriteFile(filePath, htmlString);
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
     });
 }
 
